dab5f0366513c61c8571389ec4daf9c44874a1a5
vinay-lanka <lanka.vinay2017@vitstudent.ac.in>
2020-02-09 13:40:54 +0530

    Pages

---
+++ script.js
@@ -0,0 +1,10 @@
+new TypeIt("#app", {
+  strings: ["IoT Engineer","Robotics Engineer","Electronics Hobbyist","Web Developer"],
+  speed: 75,
+  breakLines: false,
+  waitUntilVisible: true,
+  lifeLike: true,
+  loop: true,
+  loopDelay: 3000,
+  nextStringDelay: 2000
+}).go();
