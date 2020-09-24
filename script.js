575e9cea0346a2d98a5a5a69a8f2b4b82221a3f8
vinay_lanka <vinaylanka111@gmail.com>
2020-09-24 14:08:11 +0530

    update projects, blogs and resume
    fixup! update projects, blogs and resume
    
    fixup! update projects, blogs and resume

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ new TypeIt("#app", {
   lifeLike: true,
   loop: true,
   loopDelay: 3000,
-  nextStringDelay: 1000
+  nextStringDelay: 2000
 }).go();
 
 var project_div = document.getElementById("project_div");
@@ -17,28 +17,25 @@ blogs_div.style.display = "none";
 project_div.style.display = "none";
 card_div.style.display = "block";
 
-function toggleProjects() {
-  if (project_div.style.display === "none") {
+function toggle(that){
+  console.log(that.innerHTML);
+  if(that.innerHTML==="Projects"){
     project_div.style.display = "block";
+    blogs_div.style.display = "none";
     card_div.style.display = "none";
     document.getElementById("nav1").innerHTML = "Home";
-    
-  } else {
+    document.getElementById("nav2").innerHTML = "Blogs";
+  }else if (that.innerHTML==="Home"){
     project_div.style.display = "none";
-    card_div.style.display = "block";
-    document.getElementById("nav1").innerHTML = "Projects";
-  }
-}
-
-function toggleBlogs() {
-  if (blogs_div.style.display === "none") {
-    blogs_div.style.display = "block";
-    card_div.style.display = "none";
-    document.getElementById("nav2").innerHTML = "Home";
-    
-  } else {
     blogs_div.style.display = "none";
     card_div.style.display = "block";
+    document.getElementById("nav1").innerHTML = "Projects";
     document.getElementById("nav2").innerHTML = "Blogs";
+  }else if(that.innerHTML==="Blogs"){
+    blogs_div.style.display = "block";
+    card_div.style.display = "none";
+    project_div.style.display = "none";
+    document.getElementById("nav1").innerHTML = "Home";
+    document.getElementById("nav2").innerHTML = "Projects";
   }
 }
