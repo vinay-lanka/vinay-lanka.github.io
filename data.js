40454ad9147f96dcc404f84707619cc77c13d39f
vinay-lanka <vinaylanka111@gmail.com>
2022-01-31 11:10:24 +0530

    2022 refresh

---
+++ data.js
@@ -0,0 +1,75 @@
+var project_data =  [
+    {
+        "title": "Robot-Web-Suite",
+        "body": "Robot-Web-Suite is a web-based platform to remotely control your robotic devices in real-time. <br>An intuitive web interface to manipulate devices like robotic arms and rovers with a service to log points and achieve playback. <br>The system is aimed to replace the usage of teach pendants or similar devices in industrial automation. <br>Using ROS and MoveIt packages we focus on creating a hardware agnostic system that can easily be ported to any robot.",        "link": "https://www.youtube.com/watch?v=37U62tWk-zE&t=29s",
+        "imagelink": "assets/images/robotwebsuite.png"
+    },
+    {
+        "title": "Tnet",
+        "body": "Tnet is an end to end solution with a portal designed to help customers view their injection moulding machine data in an intuitive web interface.<br><br> Uses MySQL, Node.js with Express and AngularJS. Part of my summer internship at Toshiba Machine, Chennai.",
+        "link": "https://github.com/vinay-lanka/tnet",
+        "imagelink": "assets/images/toshiba.png"
+    },
+    {
+        "title": "RobIoTic Arms - A web interface for Robotics Arms (ROS/MoveIt)",
+        "body": "Implementation of an industry ready robot arm controller for arm jogging and saving positions through a web interface. <br><br> Uses ROS and MoveIt for Inverse Kinematics and Path Planning<br> An AVR microcontroller drives the servos with a PCA9685 board<br> The website is made with a Node-js backend and has an intuitive frontend to manipulate the robot.</p>",
+        "link": "https://www.youtube.com/watch?v=T3iEXmOk7j4&t=23s",
+        "imagelink": "assets/images/robioticarm.jpg"
+    },
+    {
+        "title": "Home Service Robot",
+        "body": "A Home Service Robot simulated in ROS and Gazebo. Can navigate to a maker, pick up a virtual object and drop it off at another maker.<br><br> Uses - <br> - Adaptive Monte Caro Localisation for Localisation.<br> - Gmapping for Mapping<br> - Navigation is done with move_base ros package",
+        "link": "https://github.com/vinay-lanka/Home-Service-Robot",
+        "imagelink": "assets/images/homerobotproject.gif"
+    },
+    {
+        "title": "Robot arm color sorter",
+        "body": "A Raspberry Pi based real time colour sorter based on Computer Vision (OpenCV) and the Dobot Magician arm, a robotic arm with an Android app to show statistics",
+        "link": "https://youtube.com/watch?v=BghCrTwk0t0&t=21s",
+        "imagelink": "assets/images/coloursorter.png"
+    }
+]
+var blog_data =  [
+    {
+        "title": "Autonomous Rover From Scratch (ROS) Part 1",
+        "body": " Part 1 of making an Autonomous Robot from scratch using ROS.<br> Here we look at building a basic model and getting started with the microcontroller.",
+        "link": "https://www.youtube.com/watch?v=SLDJAOEjVt4&t=434s",
+        "imagelink": "assets/images/autonomousroverpt1.jpg"
+    },
+    {
+        "title": "Autonomous Rover From Scratch (ROS) Part 2",
+        "body": " Part 2 of making an Autonomous robot from scratch using ROS. <br> This ultra-long episode covers a lot of stuff. We finally finish writing the Arduino sketch for the hardware. We briefly understand what ROS is,why it was formed and how it works. We look at the kinematics of a differential-drive robot and implement the differential-drive package in a ROS workspace.",
+        "link": "https://www.youtube.com/watch?v=n9yU7u55zGg",
+        "imagelink": "assets/images/autonomousroverpt2.jpg"
+    },
+    {
+        "title": "Making a hyper-engineered water alarm solution",
+        "body": "Raspberry Pi, ESP32, Docker, Nodejs, CI/CD, and an Android app. It has it all.",
+        "link": "https://itnext.io/making-a-hyper-engineered-water-alarm-solution-19ba5078d2bc",
+        "imagelink": "assets/images/wateralarmsystem.png"
+    },
+    {
+        "title": "Raspberry Pi Pico: A $4 beast!",
+        "body": "The Raspberry Pi Foundation’s foray into the microcontroller market. <br>(First Impressions/Tutorial)",
+        "link": "https://vinaylanka.medium.com/raspberry-pi-pico-a-4-beast-b000cc50f50a",
+        "imagelink": "assets/images/raspberrypipico.png"
+    },
+    {
+        "title": "Exploring Embedded - A Primer",
+        "body": "Exploring Embedded, a complete guide for anyone wanting to look beyond Arduino. <br>A comprehensive tutorial with links to software, explanation, and references for literature to guide you further in the field of embedded systems development.",
+        "link": "https://medium.com/@vinaylanka/exploring-embedded-a-primer-8977a51bb7af",
+        "imagelink": "assets/images/exploringembedded.png"
+    },
+    {
+        "title": "Control any Robotic Arm with ROS",
+        "body": "ROS has a steep learning curve and integrating one's robot into its ecosystem is a challenge.<br> An article on how to control any Robotic Arm with MoveIt, a powerful motion planning framework in ROS.",
+        "link": "https://blog.usejournal.com/control-any-robotic-arm-with-ros-b10a3115306c",
+        "imagelink": "assets/images/rosarmmoveit.png"
+    },
+    {
+        "title": "Development Boards: A Guide",
+        "body": "A helpful guide for people to choose the right board for their next project",
+        "link": "https://medium.com/techloop/development-boards-a-guide-ab5767f15d47",
+        "imagelink": "assets/images/devboard.jpeg"
+    }
+]
--- script.js
+++ script.js
@@ -24,14 +24,14 @@ function toggle(that){
     blogs_div.style.display = "none";
     card_div.style.display = "none";
     document.getElementById("nav1").innerHTML = "Home";
-    document.getElementById("nav2").innerHTML = "Blogs";
+    document.getElementById("nav2").innerHTML = "Videos/Blogs";
   }else if (that.innerHTML==="Home"){
     project_div.style.display = "none";
     blogs_div.style.display = "none";
     card_div.style.display = "block";
     document.getElementById("nav1").innerHTML = "Projects";
-    document.getElementById("nav2").innerHTML = "Blogs";
-  }else if(that.innerHTML==="Blogs"){
+    document.getElementById("nav2").innerHTML = "Videos/Blogs";
+  }else if(that.innerHTML==="Videos/Blogs"){
     blogs_div.style.display = "block";
     card_div.style.display = "none";
     project_div.style.display = "none";
@@ -39,3 +39,47 @@ function toggle(that){
     document.getElementById("nav2").innerHTML = "Projects";
   }
 }
+
+$(function() {
+    // console.log(project_data);
+  var html = ''
+  html+= '<span style="font-size:40px; color: #efefef;">Projects</span><br><br>';
+  html+= '<center>'
+  $.each(project_data, function(key, value) {
+    html+= '<div class="project-card">';
+    html+= '<div class="text">';
+    html+= '<h3 style="text-align: left;">' + value.title + '</h3><br>'
+    html+= value.body
+    html+= '<br><br><a href="' + value.link + '">Check it out!</a>'
+    html+= '</div>'  
+    html+= '<div>'
+    html+= '<img class="responsive" src="' + value.imagelink + '" height="300" width="533">'
+    html+= '</div>'
+    html+= '</div>'
+  });
+  html+= '</center>'
+  $("#project_div").html(html);
+  // console.log(html);
+});
+
+$(function() {
+  console.log(blog_data);
+  var blog_html = ''
+  blog_html+= '<span style="font-size:40px; color: #efefef;">Blogs</span><br><br>';
+  blog_html+= '<center>'
+  $.each(blog_data, function(key, value) {
+    blog_html+= '<div class="project-card">';
+    blog_html+= '<div class="text">';
+    blog_html+= '<h3 style="text-align: left;">' + value.title + '</h3><br>'
+    blog_html+= value.body
+    blog_html+= '<br><br><a href="' + value.link + '">Check it out!</a>'
+    blog_html+= '</div>'  
+    blog_html+= '<div>'
+    blog_html+= '<img class="responsive" src="' + value.imagelink + '"height="300" width="533">'
+    blog_html+= '</div>'
+    blog_html+= '</div>'
+  });
+  blog_html+= '</center>'
+  $("#blogs_div").html(blog_html);
+  // console.log(html);
+});
